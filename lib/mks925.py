b70427a4f3950ef37f9554c729a8a90f3920e0ec
volkswagenfeature <tristan.ritland@gmail.com>
2017-09-20 12:32:33 -0400

    Finished rough draft of code

--- lib/mks925.py
+++ lib/mks925.py
@@ -1,34 +1,15 @@
+# To interface with the serial port
 import serial
 
-class MKS925:
-    class __MKS_msg__:
-        def __init__(self, msg_types, msg_format, response_format, docstring):
-            self.msg_types       = msg_types
-            self.msg_format      = msg_format
-            self.response_format = response_format
-            self.docstring       = docstring
-    
-    class __MKS_wrapper__:
-        # TODO: These wrap a dict of MKS Messages, and 
-        # TODO: modify their dot operator to properly send the command.
-    def __init__(self,adr=253,baud=9600,port='/dev/ttyS0'):
-        # Set up vars and create serial port
-        self.baudrate = baud
-        self.serialport = port
-        self.address = adr
-        self.serialport = serial.Serial(self.serialport, 
-                                        self.baudrate)
-        
-        # Library of transmittable messages
-        self.commands = {"Address":"AD",   "Baud":"BR",   "FactoryDefault":"FD",
-                         "RSDelay":"RSD",  "Test":"TST",  "Unit":"U",
-                         "Tag":"UT",       "Type":"DT",   "FirmwareV":"FV",
-                         "HardwareV":"HV", "Origin":"MF", "Model":"MD",
-                         "Pressure":"PR1", "Serial":"SN", "Uptime":"TIM"
+# To format-check everything
+import re
 
-                        
-        self.queries
-    def __send_generic__(self, msg_type, message, parameter=""):
+# TODO: Better exception handling. At the moment my exceptions are descriptive, but non-standard.
+# TODO: Add more idiot checking for input parameters
+# TODO: Broadcast support?? Multi-sensor support?? Not currently relevant, but might want.
+
+class MKS925:
+    def __send_generic(self, msg_type, message, parameter=""):
         # Idiot checking for input parameters
         if (msg_type.find("!?") == -1 or msg_type.len() != 1):
             raise Exception("Invalid Message Type")
@@ -57,10 +38,70 @@ class MKS925:
                 raise exception("NAK received!")
                 return rawin[1:-3].split("NAK")[1]
             else:
-                raise Exception( "Unexpected response!!")
+                raise Exception( "Unexpected response")
                 return ""
         else:
-            raise Exception( "No Reply!!")
+            raise Exception( "No Reply")
             return ""
 
 
+    class __MKS_msg:
+        def __init__(self, msg_types, msg,  
+                     response_format = None, 
+                     parameter_format = None,
+                     docstring = None):
+            self.msg_types       = msg_types
+            self.msg            = msg
+            self.__doc__         = docstring
+            self.calltype        = ""
+
+            self.msg_format = "^"
+            self.response_format = "^"
+            if (parameter_format not None):
+                self.msg_format      = re.compile(msg_format)
+            if (response_format not None):
+                self.response_format = re.compile(response_format)
+
+        def __call__(self, parameter):
+            if (self.calltype.len() > 0 and self.msg_types.find(self.calltype) != -1):
+                if (self.parameter_format.match(parameter) != None):
+                    result = __send_generic(self.calltype, self.msg, parameter)
+                    # TODO: Need to be able to catch a NAK right here.
+                    if (self.response_format.fullmatch(result) != None):
+                        return result;
+                    else:
+                        raise Exception("Invalid response format")
+                else:
+                    raise Exception("Invailid Parameter Format")
+            else:
+                raise Exception("Invalid calltype passed!")
+            self.calltype = ""
+    
+    class __MKS_wrapper:
+        # These wrap a dict of MKS Messages, and return them properly via the dot operator.
+        def __init__(self, msg_type_handled, msg_dict):
+            self.msg_dict = msg_dict
+            self.msg_type_handled = msg_type_handled
+        def __getattr__(self, key):
+            msg_object = self.msg_dict[key]
+            msg_object.calltype = self.msg_type_handled
+            return msg_object
+    
+    def __init__(self,adr=253,baud=9600,port='/dev/ttyS0'):
+        # Set up vars and create serial port
+        self.baudrate = baud
+        self.serialport = port
+        self.address = adr
+        self.serialport = serial.Serial(self.serialport, 
+                                        self.baudrate)
+        
+        # Library of transmittable messages
+        self.commands = {"Address":"AD",   "Baud":"BR",   "FactoryDefault":"FD",
+                         "RSDelay":"RSD",  "Test":"TST",  "Unit":"U",
+                         "Tag":"UT",       "Type":"DT",   "FirmwareV":"FV",
+                         "HardwareV":"HV", "Origin":"MF", "Model":"MD",
+                         "Pressure":"PR1", "Serial":"SN", "Uptime":"TIM"
+
+                        
+        self.queries
+    
