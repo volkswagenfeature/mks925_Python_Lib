85dcd32feaffb94651949a2c2947836d8618986f
volkswagenfeature <tristan.ritland@gmail.com>
2017-12-02 00:57:25 -0500

    Added some documentation to virtual_terminal2.py

--- debug_virtualzed.py
+++ debug_virtualzed.py
@@ -20,29 +20,11 @@ class virtual_terminal_feature_check(unittest.TestCase):
 
     @classmethod
     def setUp(self):
-        self.virSerial = vt.virtualserialport()
-        self.master = os.open(str(self.virSerial.master_descriptor), os.O_RDWR | os.O_SYNC | os.O_DIRECT)
-        self.repetitions = 2 
-        self.max_length  = 256
-        self.char_opts   = string.printable
-        random.seed(time.time())
 
-    @classmethod
-    def tearDown(self):
-        os.close(self.master)
 
     def test_creation(self):
-        self.assertIsInstance(self.virSerial.slave_descriptor, type(os.__file__))
-        self.assertIsInstance(self.virSerial.master_descriptor, type(os.__file__))
 
     def test_master_to_slave_comms(self):
-        for test_num in range(0,self.repetitions):
-            teststring = self.randstring()
-            with self.subTest(tst_n=test_num ):
-                self.virSerial.send("teststring")
-                self.assertEqual(self.virSerial.interface.out_waiting,0)
-                self.assertEqual(self.masterInterface.in_waiting, len("teststring"))
-                self.assertEqual(self.masterInterface.read_all(), "teststring")
 
 
 if __name__ == '__main__':
--- lib/virtual_terminal2.py
+++ lib/virtual_terminal2.py
@@ -5,14 +5,36 @@
 # Trying to set it up, so that it saves what it receives to a buffer.
 # One object serves as the serial system, the other can serve as a wrapper for the buffers.
 
-# Keeping the old version in, in case we just want to stick with that.
-# BTW, I won't be able to make it Monday the 27th, I procreastinated on a bunch of stuff
-# And have to study and other stuff.
 
 
-import uuid
+
+###### Imports ######
+# Time is used to measure read timeouts
+# ...Which never exist for this library.
+# leaving it in for future implementation.
+import time
+
+# Namedtuple is used from collections for globals.
 import collections
+
+# BytesIO is used from buffers for the read/write buffers
 import io
+
+# These exceptions are stolen straight from Serial, to assure
+# identical functioning.
+from Serial import SerialException,SerialTimeoutException
+
+
+
+
+###### Global Variables ######
+# These namedtuples hold information that the virtual serial port is transmitting
+# They are global in scope, so data can be pulled out of them, and tested outside
+# of the serial class instance. This is important, because serial HAS to function EXACTLY
+# like Serial.serial, and will probably be instantiated by the class constructor.
+# As such, external stuff may not have any access to the instance, and needs these
+# to access the data.
+
 
 #The buffer system for in and out buffers
 property_Options   = ["port", "baudrate", "bytesize", "stopbits", 
@@ -30,29 +52,36 @@ Flags = collections.namedtuple('Flags', ['open','chars_buffered']
 
 
 ##### Constants ######
+# These are here because the serial library has them. the values are unimportant,
+# but they MUST be UNIQUE. I do this through a 2-byte representation. A more
+# elegant solution might be checking globals() for these values, somehow. but that's
+# probably overcomplicating things
+
+
 # Parity
-PARITY_NONE = uuid.uuid4()
-PARITY_EVEN = uuid.uuid4()
-PARITY_ODD  = uuid.uuid4()
-PARITY_MARK = uuid.uuid4()
-PARITY_SPACE= uuid.uuid4()
+PARITY_NONE = b'P0'
+PARITY_EVEN = b'P1'
+PARITY_ODD  = b'P2'
+PARITY_MARK = b'Pm'
+PARITY_SPACE= b'P_'
 
 # Stop Bits
-STOPBITS_ONE            = uuid.uuid4()
-STOPBITS_ONE_POINT_FIVE = uuid.uuid4()
-STOPBITS_TWO            = uuid.uuid4()
+STOPBITS_ONE            = b'S1'
+STOPBITS_ONE_POINT_FIVE = b'S3'
+STOPBITS_TWO            = b'S2'
 
 # Byte Size
-FIVEBITS  = uuid.uuid4()
-SIXBITS   = uuid.uuid4()
-SEVENBITS = uuid.uuid4()
-EIGHTBITS = uuid.uuid4()
+FIVEBITS  = b'B5'
+SIXBITS   = b'B6'
+SEVENBITS = b'B7'
+EIGHTBITS = b'B8'
 
 class Serial:
     def __init__(self, port=None, baudrate=9600, bytesize=EIGHTBITS, 
-             stopbits=STOPBITS_ONE, timeout=None, xonxoff=False, 
+             stopbits=STOPBITS_ONE, timeout=0, xonxoff=False, 
              rtscts=False, write_timeout=None, dsrdtr=False, 
              inter_byte_timeout=None):
+        # Just plug all of these into the global properties namedtuple.
         properties.port = port
         properties.baudrate = baudrate
         properties.bytesize = bytesize
@@ -63,7 +92,8 @@ class Serial:
         properties.write_timeout = timeout
         properties.dsrdtr = dsrdtr
         properties.inter_byte_timeout = inter_byte_timeout
-        
+
+        # These require a bit of handling, not all of which is implemented yet.
         Flags.open = True
         Flags.chars_buffered = 0
 
@@ -72,9 +102,20 @@ class Serial:
         chars_buffered += len(data)
 
     def read( self, size=1 ):
-        buffers.Rx_to_Tx.read( size )
+        data = buffers.Rx_to_Tx.read( size )
+        if (timeout!=0 || timeout!=None || len(data) == size):
+            return data
+        else:
+            # TODO: Handle timeout code here.
+            # Remember, timeout=None waits until enough bytes have been received.
+            # This is the library default behavior.
+
+            # timeout=0 never waits, just returns what it has.
+            # This is my currently implemented behavior.
+
+            # timeout=n waits float n secconds.
+
+            raise NotImplementedError("Timeouts are not properly implenented yet!")
 
     def flush( self ):
         chars_buffered = 0
-
-
